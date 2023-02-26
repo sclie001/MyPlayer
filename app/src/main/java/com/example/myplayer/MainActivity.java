258f18f18e48e37d7be759c83acb4cba17a7a410
Stephanie Cliett <sclie001@fiu.edu>
2023-02-26 16:56:10 -0500

    attached adapter to the RecyclerView

--- app/src/main/java/com/example/myplayer/MainActivity.java
+++ app/src/main/java/com/example/myplayer/MainActivity.java
@@ -16,15 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        TextView title = findViewById(R.id.song_title_textView);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        /*TextView title = findViewById(R.id.song_title_textView);
         TextView artist = findViewById(R.id.song_artist_textView);*/
+
 
         ArrayList<Song> songs = new ArrayList<>();
 
         songs.add(new Song("Haunted", "Paul Van Dyk", R.raw.haunted));
         songs.add(new Song("Save the last trance for me", "Paul Oakenfold",
                 R.raw.save_the_last_trance_for_me));
+
+        CustomAdapter adapter = new CustomAdapter(songs);
+        
+        //set CustomAdapter as the adapter for the RecyclerView
+        recyclerView.setAdapter(adapter);
 
 
 
