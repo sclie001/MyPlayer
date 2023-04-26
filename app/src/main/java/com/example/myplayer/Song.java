18826c74cf62fb54ffb82054187c1148fbc26082
Stephanie Cliett <sclie001@fiu.edu>
2023-04-26 12:37:22 -0400

    added constructor for a song that has an image

--- app/src/main/java/com/example/myplayer/Song.java
+++ app/src/main/java/com/example/myplayer/Song.java
@@ -5,12 +5,34 @@ public class Song{
     private String artist;
     private String title;
     private final int audioResourceId;
+    private int imageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
 
+    /**
+     * Create a new Song object
+     * @param title title of the song
+     * @param artist artist of the song
+     * @param audioResourceId audio resource id for the audio file associated with the song
+     */
     public Song(String title, String artist, int audioResourceId){
         this.title =  title;
         this.artist = artist;
         this.audioResourceId = audioResourceId;
 
+    }
+
+    /**
+     * Create a new Song object
+     * @param title title of the song
+     * @param artist artist of the song
+     * @param audioResourceId audio resource id for the audio file associated with the song
+     * @param imageResourceId image resource id for the image file associated with the song
+     */
+    public Song(String title, String artist, int audioResourceId, int imageResourceId){
+        this.title =  title;
+        this.artist = artist;
+        this.audioResourceId = audioResourceId;
+        this.imageResourceId = imageResourceId;
     }
 
     public String getArtist() {
